37649505e9f16deea688f11674927b6bbc2a5151
Francisco Garcia <ciscog11@msn.com>
2016-04-14 08:14:56 -0600

    added meta, h1, h2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>The Candid Appetite</title>
+	</head>
+	<body>
+		<h1>Phase 1</h1>
+			<h2>1. Front End</h2>
+	</body>
+</html>
